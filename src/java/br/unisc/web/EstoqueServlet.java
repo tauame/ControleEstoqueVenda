ee565a31a21dab6135ea285b1596b3a4bb6c43f8
Eduardo <Eduardo@dellinspiron>
2014-09-05 01:12:36 -0300

    Merge origin/master
    Conflicts:
    	src/java/br/unisc/estoquevenda/Teste.java

--- src/java/br/unisc/web/EstoqueServlet.java
+++ src/java/br/unisc/web/EstoqueServlet.java
@@ -27,15 +27,7 @@ public class EstoqueServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EstoqueServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EstoqueServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println("Use os links de navegação!");
         }
     }
 
--- src/java/br/unisc/web/ProdutoServlet.java
+++ src/java/br/unisc/web/ProdutoServlet.java
@@ -27,15 +27,7 @@ public class ProdutoServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProdutoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProdutoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println("Use os links de navegação!");
         }
     }
 

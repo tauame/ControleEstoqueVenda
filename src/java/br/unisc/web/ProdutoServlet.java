55d08dfb9648c98d9be62c03afeb2f0eea753bf6
Eduardo <Eduardo@dellinspiron>
2014-09-05 07:46:56 -0300

    Merge origin/master
    Conflicts:
    	src/java/br/unisc/estoquevenda/Teste.java

--- src/java/br/unisc/web/ProdutoServlet.java
+++ src/java/br/unisc/web/ProdutoServlet.java
@@ -61,9 +61,8 @@ public class ProdutoServlet extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             out.println("Chamou POST - ProdutoServelet.doPost (ProdutoServelet.java).</br>");
             Produto prod = new Produto();
-            // prod.setCod_produto(Integer.valueOf(request.getParameter("cod_produto")));
+            prod.setNome(request.getParameter("nome"));            
             prod.setCod_barras(Integer.valueOf(request.getParameter("cod_barras")));
-            prod.setNome(request.getParameter("nome"));
             prod.setFabricante(request.getParameter("fabricante"));
             prod.setModelo(request.getParameter("modelo"));
         
